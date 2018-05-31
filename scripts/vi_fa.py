7dea8b9205354c2c78545b4825ef26fc6e628fe8
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2018-05-31 19:35:11 +0100

    Add FA VI example

---
+++ scripts/vi_fa.py
@@ -0,0 +1,38 @@
+import numpy as np
+import tensorflow as tf
+from lab.tf import B
+from stheno.tf import Normal
+
+from wbml.vi import QNormalDiag, elbo
+
+B.set_default_dtype(np.float64)
+s = tf.Session()
+
+# Define a prior and likelihood and generate data.
+prior = Normal(B.eye(2))
+lik_noise = Normal(.1 * B.eye(2))
+z_true = s.run(prior.sample())
+y = s.run((lik_noise + z_true).sample(100))
+
+# Define likelihood and q distribution.
+lik = lik_noise + y
+pars = QNormalDiag.random_init_pars(2)
+q = QNormalDiag(*pars)
+
+# Construct objective.
+obj = -elbo(lik, prior, q)
+
+# Optimise.
+opt = tf.train.AdamOptimizer(5e-2).minimize(obj)
+s.run(tf.global_variables_initializer())
+for i in range(5000):
+    _, val = s.run([opt, obj])
+    print(i, val)
+
+# Print result.
+print('True', z_true.flatten())
+sample = q.sample(1000)
+z_est = B.mean(sample, axis=1)
+z_est_std = B.mean((sample - z_est[:, None]) ** 2, axis=1) ** .5
+print('Mean estimate', s.run(z_est))
+print('Std estimate', s.run(z_est_std))
--- wbml/vi.py
+++ wbml/vi.py
@@ -2,6 +2,7 @@
 
 from __future__ import absolute_import, division, print_function
 
+import tensorflow as tf
 from lab import B
 from stheno import Normal, Diagonal
 from wbml import create_var
@@ -21,7 +22,7 @@ class QNormal:
         return self._d.sample(num_samples)
 
     def log_pdf(self, samples):
-        return self._d.log_pdf(samples)
+        return B.sum(self._d.log_pdf(samples))
 
 
 class QNormalDiag:
@@ -29,21 +30,24 @@ class QNormalDiag:
     def random_init_pars(d=2):
         return create_var([d, 1]), create_var([d])
 
-    def __init__(self, mu, s2):
+    def __init__(self, mu, log_s2):
         self.mu = mu
-        self.s2 = s2
-        self._d = Normal(Diagonal(B.exp(s2)), mu)
+        self.log_s2 = log_s2
+        self._d = Normal(Diagonal(B.exp(log_s2)), mu)
 
     def sample(self, num_samples=1, noise=None):
         return self._d.sample(num_samples, noise)
 
     def log_pdf(self, samples):
-        return self._d.log_pdf(samples)
+        return B.sum(self._d.log_pdf(samples))
 
 
-def elbo(lik, p, q, num_samples=100):
+def elbo(lik, p, q, num_samples=1):
+    def lik_fun(z):
+        return B.sum(lik.log_pdf(z[:, None]))
+
     samples = q.sample(num_samples)
-    log_lik = B.sum(lik.log_pdf(samples))
+    log_lik = B.sum(tf.map_fn(lik_fun, B.transpose(samples)))
     log_prior = B.sum(p.log_pdf(samples))
     log_q = B.sum(q.log_pdf(samples))
     return (log_lik + log_prior - log_q) / num_samples
