cf66e6f12f69f51bbd17c35ab89d6ddd1491f95d
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2018-06-04 23:36:36 +0100

    Simplify implementation ELBO

--- wbml/vi.py
+++ wbml/vi.py
@@ -12,19 +12,19 @@ dispatch = Dispatcher()
 __all__ = ['elbo']
 
 
+def map_fn_columns(f, xs):
+    return tf.map_fn(lambda x: f(x[:, None]), B.transpose(xs))
+
+
 @dispatch(object, object, object, [object])
 def elbo(lik, p, q, num_samples=1):
     samples = q.sample(num_samples)
-    log_lik = B.sum(tf.map_fn(lambda z: lik(z[:, None]),
-                              B.transpose(samples)))
-    log_prior = B.sum(p.log_pdf(samples))
-    log_q = B.sum(q.log_pdf(samples))
-    return (log_lik - log_prior + log_q) / num_samples
+    log_lik = B.mean(map_fn_columns(lik, samples))
+    log_prior = B.mean(p.log_pdf(samples))
+    log_q = B.mean(q.log_pdf(samples))
+    return log_lik - log_prior + log_q
 
 
 @dispatch(object, Normal, Normal, [object])
 def elbo(lik, p, q, num_samples=1):
-    samples = q.sample(num_samples)
-    log_lik = B.sum(tf.map_fn(lambda z: lik(z[:, None]),
-                              B.transpose(samples)))
-    return log_lik / num_samples - p.kl(q)
+    return B.mean(map_fn_columns(lik, q.sample(num_samples))) - p.kl(q)
