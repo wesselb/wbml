40092d281a84512fbb89f0d72b70714afbfcd40e
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2018-06-04 22:46:23 +0100

    Simplify elbo

--- wbml/vi.py
+++ wbml/vi.py
@@ -7,11 +7,8 @@ from lab.tf import B
 
 
 def elbo(lik, p, q, num_samples=1):
-    def lik_fun(z):
-        return B.sum(lik.log_pdf(z[:, None]))
-
     samples = q.sample(num_samples)
-    log_lik = B.sum(tf.map_fn(lik_fun, B.transpose(samples)))
+    log_lik = B.sum(tf.map_fn(lambda z: lik(z[:, None]), B.transpose(samples)))
     log_prior = B.sum(p.log_pdf(samples))
     log_q = B.sum(q.log_pdf(samples))
     return (log_lik - log_prior + log_q) / num_samples
