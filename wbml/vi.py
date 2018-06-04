d1109bdcb05689e485f587765c81bbb3baa22f4e
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2018-06-04 23:37:24 +0100

    Rename map_fn_columns to map_cols

--- wbml/vi.py
+++ wbml/vi.py
@@ -12,14 +12,14 @@ dispatch = Dispatcher()
 __all__ = ['elbo']
 
 
-def map_fn_columns(f, xs):
+def map_cols(f, xs):
     return tf.map_fn(lambda x: f(x[:, None]), B.transpose(xs))
 
 
 @dispatch(object, object, object, [object])
 def elbo(lik, p, q, num_samples=1):
     samples = q.sample(num_samples)
-    log_lik = B.mean(map_fn_columns(lik, samples))
+    log_lik = B.mean(map_cols(lik, samples))
     log_prior = B.mean(p.log_pdf(samples))
     log_q = B.mean(q.log_pdf(samples))
     return log_lik - log_prior + log_q
@@ -27,4 +27,4 @@ def elbo(lik, p, q, num_samples=1):
 
 @dispatch(object, Normal, Normal, [object])
 def elbo(lik, p, q, num_samples=1):
-    return B.mean(map_fn_columns(lik, q.sample(num_samples))) - p.kl(q)
+    return B.mean(map_cols(lik, q.sample(num_samples))) - p.kl(q)
