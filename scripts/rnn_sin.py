d023b5ae4e2f5b6ee406ebdd101cb36e8383fd71
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2018-06-06 13:32:43 +0100

    Learn sine with RNN

---
+++ scripts/rnn_sin.py
@@ -0,0 +1,41 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+from lab.tf import B
+
+from wbml.data import normalise_01
+from wbml import rnn
+
+x = np.linspace(-20, 20, 80, dtype=np.float32)[:, None, None]
+y = np.sin(x)
+
+# Normalise inputs and outputs.
+x, y = normalise_01(x, y)
+
+# Split
+i = int(x.shape[0] * .5)
+x_train = x[:i, :, :]
+y_train = y[:i, :, :]
+
+f = rnn(1, 1, ((10, 10),))
+y_rnn = f(x_train)
+obj = B.mean((y_train - y_rnn) ** 2)
+
+s = tf.Session()
+opt = tf.train.AdamOptimizer(1e-2).minimize(obj)
+s.run(tf.global_variables_initializer())
+
+for i in range(3000):
+    _, val = s.run([opt, obj])
+    if i % 100 == 0:
+        print(i, val)
+
+y_learned = s.run(f(x)).flatten()
+x, y = x.flatten(), y.flatten()
+x_train, y_train = x_train.flatten(), y_train.flatten()
+plt.figure()
+plt.plot(x, y, label='True', ls='-', marker='o')
+plt.plot(x_train, y_train, label='Observed', marker='o', ls='none')
+plt.plot(x, y_learned, label='Learned', marker='x')
+plt.legend()
+plt.show()
