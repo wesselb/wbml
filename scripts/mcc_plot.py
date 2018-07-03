760ff76d3da28f17d86c23fec4ab7cfacd997e99
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2018-07-03 21:07:38 +0100

    Improve plot

--- scripts/mcc_plot.py
+++ scripts/mcc_plot.py
@@ -1,12 +1,17 @@
-import matplotlib.pyplot as plt
 import re
 import sys
+
+import matplotlib.pyplot as plt
 import numpy as np
 
 # Read what is piped to the program.
-out = ''
-for line in sys.stdin:
-    out += line
+fns = sys.argv[1:]
+
+# Read files.
+contents = []
+for fn in fns:
+    with open(fn, 'r') as f:
+        contents.append(f.read())
 
 
 def extract(string, n=1, period=1):
@@ -15,68 +20,97 @@ def extract(string, n=1, period=1):
     return np.array(floats[n - 1::period])
 
 
-splits = extract(out, 1, 15).astype(int)
-weeks = extract(out, 2, 15).astype(int)
+def content_to_out(content):
+    """Extract fields from content."""
+    out = {}
+    out['splits'] = extract(content, 1, 15).astype(int)
+    out['weeks'] = extract(content, 2, 15).astype(int)
 
-# Extract OLMM.
-model_means = extract(out, 3, 15)
-model_lowers = extract(out, 4, 15)
-model_uppers = extract(out, 5, 15)
-model_full = extract(out, 6, 15)
-model_rmse = extract(out, 7, 15)
+    # Extract OLMM.
+    out['model_means'] = extract(content, 3, 15)
+    out['model_lowers'] = extract(content, 4, 15)
+    out['model_uppers'] = extract(content, 5, 15)
+    out['model_full'] = extract(content, 6, 15)
+    out['model_rmse'] = extract(content, 7, 15)
 
-# Extract LW.
-lw_means = extract(out, 8, 15)
-lw_lowers = extract(out, 9, 15)
-lw_uppers = extract(out, 10, 15)
-lw_rmse = extract(out, 11, 15)
+    # Extract LW.
+    out['lw_means'] = extract(content, 8, 15)
+    out['lw_lowers'] = extract(content, 9, 15)
+    out['lw_uppers'] = extract(content, 10, 15)
+    out['lw_rmse'] = extract(content, 11, 15)
 
-# Extract EB.
-eb_means = extract(out, 12, 15)
-eb_lowers = extract(out, 13, 15)
-eb_uppers = extract(out, 14, 15)
-eb_rmse = extract(out, 15, 15)
+    # Extract EB.
+    out['eb_means'] = extract(content, 12, 15)
+    out['eb_lowers'] = extract(content, 13, 15)
+    out['eb_uppers'] = extract(content, 14, 15)
+    out['eb_rmse'] = extract(content, 15, 15)
+    return out
 
-print('Average scores ({} weeks):'.format(weeks[0]))
-print('  OLMM:')
-print('    NLML: {:-6.0f}'.format(np.mean(model_means)))
-print('    RMSE: {:-6.2f}'.format(np.mean(model_rmse)))
-print('  LW:')
-print('    NLML: {:-6.0f}'.format(np.mean(lw_means)))
-print('    RMSE: {:-6.2f}'.format(np.mean(lw_rmse)))
-print('  EB:')
-print('    NLML: {:-6.0f}'.format(np.mean(eb_means)))
-print('    RMSE: {:-6.2f}'.format(np.mean(eb_rmse)))
 
-plt.figure(figsize=(10, 5))
-plt.title('{} Weeks'.format(weeks[0]))
+# Extract all.
+outs = [content_to_out(content) for content in contents]
+
+plt.figure(figsize=(15, 10))
+plt.title('File(s): {}'.format(', '.join(fns)))
 
 # Draw grid.
-for i in splits:
+for i in outs[0]['splits']:
     plt.axvline(i, 0, 1, c='grey', lw=1.0, linestyle=':')
 
-# Connect bounds.
-for i, lower, upper in zip(splits, model_lowers, model_uppers):
-    plt.plot([i, i], [lower, upper], c='tab:blue', lw=1.0)
-for i, lower, upper in zip(splits + 0.2, lw_lowers, lw_uppers):
-    plt.plot([i, i], [lower, upper], c='tab:red', lw=1.0)
-for i, lower, upper in zip(splits + 0.4, eb_lowers, eb_uppers):
-    plt.plot([i, i], [lower, upper], c='tab:green', lw=1.0)
+gap = 0.3
+for n, (fn, out) in enumerate(zip(fns, outs)):
+    shift = gap * n / len(outs)
 
-# Draw bounds.
-size = 50
-plt.scatter(splits, model_lowers, marker='_', s=size, c='tab:blue')
-plt.scatter(splits, model_uppers, marker='_', s=size, c='tab:blue')
-plt.scatter(splits + 0.2, lw_lowers, marker='_', s=size, c='tab:red')
-plt.scatter(splits + 0.2, lw_uppers, marker='_', s=size, c='tab:red')
-plt.scatter(splits + 0.4, eb_lowers, marker='_', s=size, c='tab:green')
-plt.scatter(splits + 0.4, eb_uppers, marker='_', s=size, c='tab:green')
+    print('{}:'.format(fn))
+    print('  OLMM:')
+    print('    NLML: {:-6.0f}'.format(np.mean(out['model_means'])))
+    print('    RMSE: {:-6.2f}'.format(np.mean(out['model_rmse'])))
+    print('  LW:')
+    print('    NLML: {:-6.0f}'.format(np.mean(out['lw_means'])))
+    print('    RMSE: {:-6.2f}'.format(np.mean(out['lw_rmse'])))
+    print('  EB:')
+    print('    NLML: {:-6.0f}'.format(np.mean(out['eb_means'])))
+    print('    RMSE: {:-6.2f}'.format(np.mean(out['eb_rmse'])))
 
-# Draw means.
-plt.scatter(splits, model_means, label='LMM', marker='o', c='tab:blue')
-plt.scatter(splits + 0.2, lw_means, label='LW', marker='o', c='tab:red')
-plt.scatter(splits + 0.4, eb_means, label='EB', marker='o', c='tab:green')
+    # Connect bounds.
+    for i, lower, upper in zip(out['splits'] + shift,
+                               out['model_lowers'],
+                               out['model_uppers']):
+        plt.plot([i, i], [lower, upper], c='tab:blue', lw=1.0)
+    for i, lower, upper in zip(out['splits'] + gap + shift,
+                               out['lw_lowers'],
+                               out['lw_uppers']):
+        plt.plot([i, i], [lower, upper], c='tab:red', lw=1.0)
+    for i, lower, upper in zip(out['splits'] + 2 * gap + shift,
+                               out['eb_lowers'],
+                               out['eb_uppers']):
+        plt.plot([i, i], [lower, upper], c='tab:green', lw=1.0)
+
+    # Draw bounds.
+    size = 50
+    plt.scatter(out['splits'] + shift, out['model_lowers'],
+                marker='_', s=size, c='tab:blue')
+    plt.scatter(out['splits'] + shift, out['model_uppers'],
+                marker='_', s=size, c='tab:blue')
+    plt.scatter(out['splits'] + gap + shift, out['lw_lowers'],
+                marker='_', s=size, c='tab:red')
+    plt.scatter(out['splits'] + gap + shift, out['lw_uppers'],
+                marker='_', s=size, c='tab:red')
+    plt.scatter(out['splits'] + 2 * gap + shift, out['eb_lowers'],
+                marker='_', s=size, c='tab:green')
+    plt.scatter(out['splits'] + 2 * gap + shift, out['eb_uppers'],
+                marker='_', s=size, c='tab:green')
+
+    # Draw means.
+    size = 20
+    plt.scatter(out['splits'] + shift, out['model_means'],
+                label='LMM', marker='o', s=size, c='tab:blue')
+    plt.scatter(out['splits'] + gap + shift, out['lw_means'],
+                label='LW', marker='o', s=size, c='tab:red')
+    plt.scatter(out['splits'] + 2 * gap + shift, out['eb_means'],
+                label='EB', marker='o', s=size, c='tab:green')
+    if n == 0:
+        plt.legend()
 
 plt.ylim(0, 15000)
-plt.legend()
 plt.show()
