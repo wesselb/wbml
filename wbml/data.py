27b95344af25df39f1b10e7c2193210714b365fb
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2018-10-01 14:49:41 +0100

    Add docstrings to wbml.data

--- wbml/data.py
+++ wbml/data.py
@@ -17,6 +17,14 @@ dispatch = Dispatcher()
 
 @dispatch(object)
 def ensure_numeric(x):
+    """Ensure that an object is numeric.
+
+    Args:
+        x (object): Object to make numeric.
+
+    Returns:
+        tensor: Input as tensor.
+    """
     return np.array(x)
 
 
@@ -26,15 +34,25 @@ def ensure_numeric(x):
 
 
 class Data(Referentiable):
-    dispatch = Dispatcher(in_class=Self)
+    """Data set.
+
+    Args:
+        x (tensor): Inputs.
+        y (tensor): Outputs.
+    """
+
+    _dispatch = Dispatcher(in_class=Self)
 
     def __init__(self, x, y):
-        self.x = ensure_numeric(x)
-        self.y = ensure_numeric(y)
+        x = ensure_numeric(x)
+        y = ensure_numeric(y)
 
-        # Ensure that `y` is a two-dimensional object.
-        if B.rank(y) == 1:
-            self.y = ensure_numeric(self.y)[:, None]
+        # Ensure that `x` and `y` are at least two-dimensional.
+        x = x[:, None] if B.rank(x) == 1 else x
+        y = y[:, None] if B.rank(y) == 1 else y
+
+        self.x = x
+        self.y = y
 
     @property
     def n(self):
@@ -50,43 +68,115 @@ class Data(Referentiable):
         return Data(self.x[item], self.y[item])
 
     def shuffle(self):
+        """Shuffle the data.
+
+        Returns:
+            :class:`.data.Data`: Shuffled data.
+        """
         inds = np.random.permutation(self.n)
         return Data(self.x[inds], self.y[inds])
 
-    @dispatch(float)
+    @_dispatch(float)
     def split(self, f):
+        """Split the data.
+
+        Args:
+            point (int or float): Point to split the data at. Can either be an
+                int, representing an index, or a float, representing a
+                fractional point.
+
+        Returns:
+            tuple[:class:`.data.Data`]: First and second part of the data.
+        """
         return self.split(int(np.round(self.n * f)))
 
-    @dispatch(int)
+    @_dispatch(int)
     def split(self, i):
         return Data(self.x[:i], self.y[:i]), \
                Data(self.x[i:], self.y[i:])
 
     def truncate(self, x):
+        """Truncate the data.
+
+        Args:
+            point (int or float): Point to truncate the data at. Can either
+                be an int, representing an index, or a float, representing a
+                fractional point.
+
+        Returns:
+            :class:`.data.Data`: Truncated data.
+        """
         return self.split(x)[0]
 
-    def normalise(self):
-        mean_x = np.mean(self.x, axis=0)[None, :]
-        std_x = np.std(self.x, axis=0)[None, :]
-        return Data((self.x - mean_x) / std_x, self.y)
+    def normalise(self, ref=None, unnormaliser=False):
+        """Normalise data.
+
+        Args:
+            ref (:class:`.data.Data`, optional): Reference to normalise with
+                respect to. Defaults to itself.
+            unnormaliser (bool, optional): Also return a function to
+                unnormalise data. Defaults to `False`.
+
+        Returns:
+            :class:`.data.Data`: Normalised data. Can further return a
+                function to unnormalise the data.
+        """
+        ref = self if ref is None else ref
+        y_mean = np.nanmean(ref.y, axis=0, keepdims=True)
+        y_std = np.nanstd(ref.y, axis=0, keepdims=True)
+        data = Data(self.x, (self.y - y_mean) / y_std)
+
+        # Also return function to unnormalise data if asked for.
+        if unnormaliser:
+            def unnormalise(d):
+                return Data(d.x, d.y * y_std + y_mean)
+
+            return data, unnormalise
+        else:
+            return data
 
     def cast(self, dtype):
+        """Cast the data to a particular data type.
+
+        Args:
+            dtype (dtype): Date type to cast to.
+
+        Returns:
+            :class:`.data.Data`: Data casted to `dtype`.
+        """
         return Data(B.cast(self.x, dtype), B.cast(self.y, dtype))
 
 
 class CSVReader:
+    """Read and parse a CSV."""
+
     def __init__(self):
         self._fields = []
         self._groups = []
         self._current_group = 0
 
     def set_field_group(self, name, matrix=True):
+        """Create a group of fields.
+
+        Args:
+            name (str): Name of group.
+            matrix (bool, optional): Output group as a matrix instead of a
+                dictionary. Defaults to `True`.
+        """
         if name not in [x['name'] for x in self._groups]:
             self._groups.append({'name': name,
                                  'matrix': matrix})
         self._current_group = name
 
     def add_field(self, column_name, type, output_name=None):
+        """Add a field.
+
+        Args:
+            column_name (str): Name of column.
+            type (type or function): Type of column. Can also be a function
+                that convert the value into the right type.
+            output_name (str, optional): Change the name of the column.
+        """
         # If there is no field group yet, make one called zero.
         if len(self._groups) == 0:
             self.set_field_group(0, matrix=True)
@@ -109,6 +199,14 @@ class CSVReader:
                              'group': self._current_group})
 
     def read(self, file_name, delimiter=',', cache=False):
+        """Read and parse a CSV.
+
+        Args:
+            file_name (str): File name.
+            delimiter (str, optional): Delimiter. Defaults to a comma.
+            cache (bool, optional): Use a cache if available. Defaults to
+                `False`.
+        """
         cache_name = '{}.cache'.format(file_name)
 
         # Check for cached data.
@@ -156,6 +254,14 @@ class CSVReader:
 
 @dispatch([object])
 def normalise_01(*xs):
+    """Normalise objects to [0, 1].
+
+    Args:
+        *xs: Objects to normalise.
+
+    Returns:
+        list: Normalised objects.
+    """
     return [normalise_01(x) for x in xs]
 
 
@@ -174,6 +280,14 @@ def normalise_01(x):
 
 @dispatch([object])
 def normalise_norm(*xs):
+    """Normalise objects to N(0, 1).
+
+    Args:
+        *xs: Objects to normalise.
+
+    Returns:
+        list: Normalised objects.
+    """
     return [normalise_norm(x) for x in xs]
 
 
