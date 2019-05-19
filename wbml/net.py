7aa2fb9f8b580057fe783546ea4feddff04b02ef
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2019-05-19 22:55:48 +0100

    Rework of WBML.net

--- wbml/net.py
+++ wbml/net.py
@@ -4,88 +4,80 @@ from __future__ import absolute_import, division, print_function
 
 from abc import abstractmethod, ABCMeta
 
-from lab import B
+import lab.tensorflow as B
+import six
+import tensorflow as tf
 
-from .util import vars32, inv_perm, identity
 
-
-class Layer(object):
-    __metaclass__ = ABCMeta
+class Layer(six.with_metaclass(ABCMeta, object)):
+    """A layer."""
 
     @abstractmethod
-    def initialise(self, input_size, vars):
-        pass
+    def initialise(self, input_size, vs):
+        """Initialise the weights of the layer.
+
+        Args:
+            input_size (int): Size of the input.
+            vs (:class:`varz.Vars`): Variable container.
+        """
 
     @abstractmethod
     def __call__(self, x):
         pass
 
     @abstractmethod
-    def weights(self):
-        pass
-
-    @abstractmethod
     def num_weights(self, input_size):
-        pass
+        """Get the number of weights used in the layer.
+
+        Args:
+            input_size (int): Size of the input.
+
+        Returns:
+            int: Number of weights.
+        """
 
 
-class Identity(Layer):
-    def __init__(self):
-        pass
-
-    def initialise(self, *arg):
-        pass
-
-    def weights(self):
-        return []
+class AutoBatchLayer(six.with_metaclass(ABCMeta, Layer)):
+    """A layer that automatically handles batched calls."""
 
     def __call__(self, x):
-        return identity(x)
-
-    def num_weights(self, input_size):
-        return 0
-
-
-class FeedForwardLayer(Layer):
-    __metaclass__ = ABCMeta
-
-    def __call__(self, *xs):
-        # Stack inputs.
-        x = B.concat(xs, axis=0)
+        # Handle the rank one case.
+        if B.rank(x) == 1:
+            x = B.expand_dims(x, 1)
 
         if B.rank(x) == 2:
             return self._apply(x)
-        else:
+        elif B.rank(x) == 3:
             # Shape into one big matrix for efficient application.
-            x = B.transpose(x, [1, 2, 0])
-            shape = B.shape(x)
-            x = B.reshape(x, [B.shape(x)[0], -1])
+            batch_size, n, m = B.shape(x)
+            x = B.reshape(x, batch_size * n, m)
 
             # Apply.
             x = self._apply(x)
 
-            # Shape back.
-            x = B.reshape(x, [self.width, shape[1], shape[2]])
-            return B.transpose(x, inv_perm([1, 2, 0]))
+            # Shape back and return.
+            return B.reshape(x, batch_size, n, m)
+        else:
+            raise ValueError('I do not know how to handle inputs of rank {}.'
+                             ''.format(B.rank(x)))
 
     @abstractmethod
     def _apply(self, x):
         pass
 
 
-class Normalise(FeedForwardLayer):
+class Normalise(AutoBatchLayer):
+    """Batch-norm layer."""
+
     def __init__(self):
         self.width = None
 
-    def initialise(self, input_size, vars):
+    def initialise(self, input_size, vs):
         self.width = input_size
-
-    def weights(self):
-        return []
 
     def _apply(self, x):
         mean = B.mean(x, axis=0)[None, :]
-        std = B.mean((x - mean) ** 2, axis=0)[None, :] ** .5
+        std = B.std(x, axis=0)[None, :]
         return (x - mean) / (std + B.epsilon)
 
     def num_weights(self, input_size):
@@ -93,200 +85,286 @@ class Normalise(FeedForwardLayer):
         return 0
 
 
-class Dense(FeedForwardLayer):
-    def __init__(self, width, nonlinearity=None):
+class Linear(AutoBatchLayer):
+    """Linear layer.
+
+    Args:
+        width (int): Width of the layer.
+    """
+
+    def __init__(self, width):
         self.width = width
         self.A = None
         self.b = None
-        self.nonlinearity = B.relu if nonlinearity is None else nonlinearity
 
-    def initialise(self, input_size, vars):
-        self.A = vars.get(shape=[self.width, input_size])
-        self.b = vars.get(shape=[self.width, 1])
+    def initialise(self, input_size, vs):
+        self.A = vs.get(shape=[input_size, self.width])
+        self.b = vs.get(shape=[1, self.width])
 
     def _apply(self, x):
-        return self.nonlinearity(B.matmul(self.A, x) + self.b)
-
-    def weights(self):
-        return B.concat([B.reshape(self.A, [-1]),
-                         B.reshape(self.b, [-1])], axis=0)
+        return B.matmul(x, self.A) + self.b
 
     def num_weights(self, input_size):
-        return self.width * (input_size + 1)
+        return input_size * self.width + self.width
 
 
-class Linear(Dense):
-    def __init__(self, width):
-        Dense.__init__(self, width, nonlinearity=identity)
+class Activation(AutoBatchLayer):
+    """Activation layer.
+
+    Args:
+        nonlinearity (function, optional): Nonlinearity. Defaults to `B.relu`.
+    """
+
+    def __init__(self, nonlinearity=B.relu):
+        self.width = None
+        self.nonlinearity = nonlinearity
+
+    def initialise(self, input_size, vs):
+        self.width = input_size
+
+    def _apply(self, x):
+        return self.nonlinearity(x)
+
+    def num_weights(self, input_size):
+        self.width = input_size
+        return 0
 
 
-class Net(object):
-    def __init__(self, input_size, layers):
-        self.input_size = input_size
+class Net(Layer):
+    """A composition of layers.
+
+    Args:
+        layers (:class:`.net.Layer`): List of the layers to compose.
+    """
+
+    def __init__(self, *layers):
         self.layers = layers
-        self.is_initialised = False
 
-    def initialise(self, vars=vars32):
+    def initialise(self, input_size, vs):
         # Initialise layers.
-        cur_size = self.input_size
         for layer in self.layers:
-            layer.initialise(cur_size, vars)
-            cur_size = layer.width
-        self.is_initialised = True
-
-    def weights(self):
-        return B.concat([x.weights() for x in self.layers], axis=0)
+            layer.initialise(input_size, vs)
+            input_size = layer.width
 
     def __call__(self, x):
-        # Make sure the net is initialised.
-        if not self.is_initialised:
-            self.initialise()
-
-        # Apply layers.
+        # Apply layers one by one.
         for layer in self.layers:
             x = layer(x)
         return x
 
-    def num_weights(self):
+    def num_weights(self, input_size):
         num_weights = 0
-        cur_size = self.input_size
         for layer in self.layers:
-            num_weights += layer.num_weights(cur_size)
-            cur_size = layer.width
+            num_weights += layer.num_weights(input_size)
+            input_size = layer.width
         return num_weights
 
 
-class GRU(Layer):
-    def __init__(self, width, nonlinearity=None):
-        self.nonlinearity = B.tahn if nonlinearity is None else nonlinearity
-        self.width = width
-        self.f_z = Dense(self.width, nonlinearity=B.sigmoid)
-        self.f_r = Dense(self.width, nonlinearity=B.sigmoid)
-        self.f_h = Dense(self.width, nonlinearity=self.nonlinearity)
-        self.h0 = None
+class Recurrent(Layer):
+    """An recurrent layer.
 
-    def initialise(self, input_size, vars):
-        self.h0 = vars.get(shape=[self.width, 1])
-        self.f_z.initialise(self.width + input_size, vars)
-        self.f_r.initialise(self.width + input_size, vars)
-        self.f_h.initialise(self.width + input_size, vars)
+    Args:
+        cell (:class:`.net.Cell`): Cell of the recurrent layer.
+        h0 (tensor, optional): Initial hidden state. Defaults to a variable
+            that will be learned.
+    """
 
-    def __call__(self, xs):
-        batch_size = B.shape(xs)[2]
+    def __init__(self, cell, h0=None):
+        self.cell = cell
+        self.h0 = h0
 
-        def loop(prev, x):
-            prev_h, prev_y = prev
+    def initialise(self, input_size, vs):
+        if self.h0 is None:
+            self.h0 = vs.get(shape=[1, self.cell.width])
+        self.cell.initialise(input_size, vs)
 
-            # Gate logic:
-            z = self.f_z(prev_h, x)
-            r = self.f_r(prev_h, x)
-            h = (1 - z) * prev_h + z * self.f_h(r * prev_h, x)
-            y = h
+    def __call__(self, x):
+        # Put the batch dimension second.
+        if B.rank(x) == 1:
+            x = x[:, None, None]
+        elif B.rank(x) == 2:
+            x = x[:, None, :]
+        elif B.rank(x) == 3:
+            x = B.transpose(x, perm=(1, 0, 2))
+        else:
+            raise ValueError('Cannot handle inputs of rank {}.'
+                             ''.format(B.rank(x)))
 
-            return h, y
+        # Recurrently apply the cell.
+        n, batch_size, m = B.shape(x)
+        y0 = B.zeros(B.dtype(x), batch_size, self.cell.width)
+        h0 = tf.tile(self.h0, [batch_size, 1])
+        res = tf.scan(self.cell, x, initializer=(h0, y0))[1]
 
-        y0 = B.zeros((self.width, batch_size), dtype=B.dtype(xs))
-        h0 = B.tile(self.h0, (1, batch_size))
-        return B.scan(loop, xs, initializer=(h0, y0))[1]
-
-    def weights(self):
-        return B.concat([B.reshape(self.h0, [-1]),
-                         self.f_z.weights(),
-                         self.f_r.weights(),
-                         self.f_h.weights()], axis=0)
+        # Put the batch dimension first again.
+        return B.transpose(res, perm=(1, 0, 2))
 
     def num_weights(self, input_size):
-        return self.width + \
-               self.f_z.num_weights(self.width + input_size) + \
+        return self.cell.width + self.cell.num_weights(input_size)
+
+
+class Cell(six.with_metaclass(ABCMeta, object)):
+    """An abstract cell of a recurrent layer."""
+
+    @abstractmethod
+    def initialise(self, input_size, vs):
+        """Initialise the weights of the cell.
+
+        Args:
+            input_size (int): Size of the input.
+            vs (:class:`varz.Vars`): Variable container.
+        """
+
+    @abstractmethod
+    def __call__(self, prev, x):
+        pass
+
+    @abstractmethod
+    def num_weights(self, input_size):
+        """Get the number of weights used in the cell.
+
+        Args:
+            input_size (int): Size of the input.
+
+        Returns:
+            int: Number of weights.
+        """
+
+
+class GRU(Cell):
+    """A gated recurrent unit.
+
+    Args:
+        width (int): Width of the hidden.
+        nonlinearity (function, optional): Nonlinearity to use. Defaults to
+            `B.tanh`.
+    """
+
+    def __init__(self, width, nonlinearity=B.tanh):
+        self.width = width
+        self.f_z = Net(Linear(self.width), Activation(B.sigmoid))
+        self.f_r = Net(Linear(self.width), Activation(B.sigmoid))
+        self.f_h = Net(Linear(self.width), Activation(nonlinearity))
+
+    def initialise(self, input_size, vs):
+        self.f_z.initialise(self.width + input_size, vs)
+        self.f_r.initialise(self.width + input_size, vs)
+        self.f_h.initialise(self.width + input_size, vs)
+
+    def __call__(self, prev, x):
+        prev_h, _ = prev
+
+        # Gate logic:
+        z = self.f_z(B.concat(prev_h, x, axis=1))
+        r = self.f_r(B.concat(prev_h, x, axis=1))
+        h = (1 - z) * prev_h + z * self.f_h(B.concat(r * prev_h, x, axis=1))
+        y = h
+
+        return h, y
+
+    def num_weights(self, input_size):
+        return self.f_z.num_weights(self.width + input_size) + \
                self.f_r.num_weights(self.width + input_size) + \
                self.f_h.num_weights(self.width + input_size)
 
 
-class Elman(Layer):
-    def __init__(self, width, nonlinearity=None):
-        self.nonlinearity = B.sigmoid if nonlinearity is None else nonlinearity
+class Elman(Cell):
+    """An recurrent layer of the Elman type.
+
+    Args:
+        width (int): Width of the layer.
+        nonlinearity (function, optional): Nonlinearity to use. Defaults to
+            `B.tanh`.
+    """
+
+    def __init__(self, width, nonlinearity=B.tanh):
         self.width = width
-        self.f_h = Dense(self.width, nonlinearity=self.nonlinearity)
-        self.h0 = None
+        self.f_h = Net(Linear(self.width), Activation(nonlinearity))
 
-    def initialise(self, input_size, vars):
-        self.h0 = vars.get(shape=[self.width, 1])
-        self.f_h.initialise(self.width + input_size, vars)
+    def initialise(self, input_size, vs):
+        self.f_h.initialise(self.width + input_size, vs)
 
-    def __call__(self, xs):
-        batch_size = B.shape(xs)[2]
+    def __call__(self, prev, x):
+        prev_h, prev_y = prev
 
-        def loop(prev, x):
-            prev_h, prev_y = prev
+        # Gate logic:
+        h = self.f_h(B.concat(prev_h, x, axis=1))
+        y = h
 
-            # Gate logic:
-            h = self.f_h(prev_h, x)
-            y = h
-
-            return h, y
-
-        y0 = B.zeros((self.width, batch_size), dtype=B.dtype(xs))
-        h0 = B.tile(self.h0, (1, batch_size))
-        return B.scan(loop, xs, initializer=(h0, y0))[1]
-
-    def weights(self):
-        return B.concat([B.reshape(self.h0, [-1]), self.f_h.weights()], axis=0)
+        return h, y
 
     def num_weights(self, input_size):
-        return self.width + \
-               self.f_h.num_weights(self.width + input_size)
+        return self.f_h.num_weights(self.width + input_size)
 
 
-def ff(input_size, output_size, widths, nonlinearity=None, normalise=True):
+def ff(output_size, widths, nonlinearity=B.relu, normalise=True):
     """A standard feed-forward neural net.
 
     Args:
-        input_size (int): Size of input.
         output_size (int): Size of output.
         widths (tuple of int): Widths of the layers.
         nonlinearity (function, optional): Nonlinearity to use. Defaults to
-            ReLUs.
+            `B.relu`.
         normalise (bool, optional): Interleave with normalisation layers.
             Defaults to `True`.
+
+    Returns:
+        :class:`.net.Net`: Feed-forward NN.
     """
-    nonlinearity = B.relu if nonlinearity is None else nonlinearity
     layers = []
+
     for width in widths:
-        layers.append(Dense(width, nonlinearity=nonlinearity))
+        layers.append(Linear(width))
+        layers.append(Activation(nonlinearity))
+
+        # Add a batch-norm layer, if asked for.
         if normalise:
             layers.append(Normalise())
+
+    # Add a final linear layer to scale outputs.
     layers.append(Linear(output_size))
-    return Net(input_size, layers)
+    return Net(*layers)
 
 
-def rnn(input_size, output_size, widths,
-        nonlinearity=None,
+def rnn(output_size,
+        widths,
+        nonlinearity=B.relu,
         normalise=False,
         gru=True,
         final_dense=False):
     """A standard recurrent neural net.
 
     Args:
-        input_size (int): Size of input.
         output_size (int): Size of output.
         widths (tuple of int): Widths of the layers.
         nonlinearity (function, optional): Nonlinearity to use. Defaults to
-            sigmoid.
+            `B.relu`.
         normalise (bool, optional): Interleave with normalisation layers.
             Defaults to `False`.
         gru (bool, optional): Use GRU layers instead of Elman layers. Defaults
             to `True`.
         final_dense (bool, optional): Append a final dense layer. Defaults to
             `False`.
+
+    Returns:
+        :class:`.net.Net`: RNN.
     """
-    nonlinearity = B.sigmoid if nonlinearity is None else nonlinearity
     layer_type = GRU if gru else Elman
     layers = []
+
     for width in widths:
-        layers.append(layer_type(width, nonlinearity=nonlinearity))
+        layers.append(Recurrent(layer_type(width)))
+        layers.append(Activation(nonlinearity))
+
+        # Add a batch-norm layer, if asked for.
         if normalise:
             layers.append(Normalise())
+
+    # Add a final dense layer, if asked for.
     if final_dense:
-        layers.append(Dense(output_size, nonlinearity=nonlinearity))
+        layers.append(Linear(output_size))
+        layers.append(Activation(nonlinearity))
+
+    # Add a final linear layer to scale outputs.
     layers.append(Linear(output_size))
-    return Net(input_size, layers)
+    return Net(layers)
