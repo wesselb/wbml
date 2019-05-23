1c801499a599da519be04ee07bfdba2d7cc22c54
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2019-05-23 16:52:49 +0100

    Move BatchVars and test normal1d_logpdf and BatchVars

--- tests/test_util.py
+++ tests/test_util.py
@@ -3,12 +3,32 @@
 from __future__ import absolute_import, division, print_function
 
 import numpy as np
+from stheno import Normal
+import lab as B
 
 # noinspection PyUnresolvedReferences
 from . import eq, neq, lt, le, ge, gt, raises, ok, allclose
-from . import inv_perm
+from . import inv_perm, normal1d_logpdf, BatchVars
 
 
 def test_inv_perm():
     perm = np.random.permutation(10)
-    yield allclose, perm[inv_perm(perm)], np.arange(10)
+    yield allclose, perm[inv_perm(perm)], B.range(10)
+
+
+def test_normal1d_logpdf():
+    means = B.randn(3, 3)
+    covs = B.randn(3, 3) ** 2
+    x = B.randn(3, 3)
+    logpdfs = normal1d_logpdf(x, covs, means)
+    for i in range(3):
+        for j in range(3):
+            dist = Normal(covs[i:i + 1, j:j + 1], means[i:i + 1, j:j + 1])
+            yield allclose, logpdfs[i, j], dist.logpdf(x[i, j])
+
+
+def test_batchvars():
+    source = B.randn(5, 2 + 3 * 4)
+    vs = BatchVars(source=source)
+    yield allclose, vs.get(shape=(1, 2)), B.reshape(source[:, :2], 5, 1, 2)
+    yield allclose, vs.get(shape=(3, 4)), B.reshape(source[:, 2:], 5, 3, 4)
--- wbml/util.py
+++ wbml/util.py
@@ -2,7 +2,11 @@
 
 from __future__ import absolute_import, division, print_function
 
-__all__ = ['inv_perm']
+from functools import reduce
+from operator import mul
+import lab as B
+
+__all__ = ['inv_perm', 'normal1d_logpdf', 'BatchVars']
 
 
 def inv_perm(perm):
@@ -18,3 +22,43 @@ def inv_perm(perm):
     for i, p in enumerate(perm):
         out[p] = i
     return out
+
+
+def normal1d_logpdf(x, var, mean=0):
+    """Broadcast the one-dimensional normal logpdf.
+
+    Args:
+        x (tensor): Point to evaluate at.
+        var (tensor): Variances.
+        mean (tensor): Means.
+
+    Returns:
+        tensor: Logpdf.
+    """
+    return -(B.log_2_pi + B.log(var) + (x - mean) ** 2 / var) / 2
+
+
+class BatchVars(object):
+    """Extract variables from a source with a batch axis as the first axis.
+
+    Args:
+        source (tensor): Source.
+    """
+
+    def __init__(self, source):
+        self.source = source
+        self.index = 0
+
+    def get(self, shape):
+        """Get a batch of tensor of a particular shape.
+
+        Args:
+            shape (shape): Shape of tensor.
+
+        Returns:
+            tensor: Batch of tensors of shape `shape`.
+        """
+        length = reduce(mul, shape, 1)
+        res = self.source[:, self.index:self.index + length]
+        self.index += length
+        return B.reshape(res, -1, *shape)
