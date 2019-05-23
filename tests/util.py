167a10b43df4a94c08f2ed34f1d201f5fa20d6d6
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2019-05-23 18:57:36 +0100

    Add utils for tests

--- tests/util.py
+++ tests/util.py
@@ -4,8 +4,8 @@ from __future__ import absolute_import, division, print_function
 
 from nose.tools import assert_raises, assert_equal, assert_less, \
     assert_less_equal, assert_not_equal, assert_greater, \
-    assert_greater_equal, ok_
-from numpy.testing import assert_allclose
+    assert_greater_equal, ok_, assert_is_instance
+from numpy.testing import assert_allclose, assert_array_almost_equal
 
 le = assert_less_equal
 lt = assert_less
@@ -16,3 +16,5 @@ gt = assert_greater
 raises = assert_raises
 ok = ok_
 allclose = assert_allclose
+approx = assert_array_almost_equal
+isinstance = assert_is_instance
