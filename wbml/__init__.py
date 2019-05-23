fe8751e799d2d7616784474ffe3390d0f06ba268
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2019-05-23 18:59:03 +0100

    Remove lr.py from __init__.py

--- wbml/__init__.py
+++ wbml/__init__.py
@@ -4,8 +4,6 @@ from __future__ import absolute_import, division, print_function
 
 from .util import *
 from .net import *
-from .lr import *
 from .data import *
 from .vi import *
 from .lmm import *
-
