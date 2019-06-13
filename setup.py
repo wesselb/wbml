f871043e0f2690bae19b38f958b91a76c741c80d
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2019-06-13 18:44:37 +0100

    Tighten requirements and increase version

--- setup.py
+++ setup.py
@@ -5,17 +5,19 @@ from __future__ import absolute_import, division, print_function
 from setuptools import find_packages, setup
 
 requirements = ['six',
-                'numpy',
-                'scipy<=1.2.1',
+                'numpy>=1.16',
+                'scipy<1.3',
                 'autograd',
                 'tensorflow',
                 'torch',
                 'matplotlib',
                 'sklearn',
 
-                'plum',
-                'lab',
-                'stheno']
+                'plum-dispatch',
+                'backends',
+                'stheno',
+                'varz']
 
 setup(packages=find_packages(exclude=['docs']),
-      install_requires=requirements)
+      install_requires=requirements,
+      include_package_data=True)
