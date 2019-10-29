32c6663318cd23e6ac49016c423e97e3e598d764
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2019-10-29 21:31:31 +0000

    Fix deps and drop Python 2

--- setup.py
+++ setup.py
@@ -6,18 +6,13 @@ from setuptools import find_packages, setup
 
 requirements = ['six',
                 'numpy>=1.16',
-                'scipy<1.3',
-                'autograd',
-                'tensorflow',
-                'torch',
-                'matplotlib',
-                'sklearn',
 
-                'plum-dispatch',
-                'backends',
+                'plum',
+                'backends>=0.3.0',
                 'stheno',
                 'varz']
 
 setup(packages=find_packages(exclude=['docs']),
+      python_requires='>=3.5',
       install_requires=requirements,
       include_package_data=True)
