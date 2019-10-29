cc27280084f1119bf97b6f869eb0a053bd9b903f
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2019-10-29 20:03:05 +0000

    Fix tests

--- tests/util.py
+++ tests/util.py
@@ -3,10 +3,26 @@
 from __future__ import absolute_import, division, print_function
 
 from numpy.testing import assert_allclose, assert_array_almost_equal
+from stheno.matrix import Dense, dense
+from plum import Dispatcher
 
 __all__ = ['allclose', 'approx']
 
-allclose = assert_allclose
+_dispatch = Dispatcher()
+
+
+@_dispatch(object)
+def to_numpy(x):
+    return x
+
+
+@_dispatch(Dense)
+def to_numpy(x):
+    return dense(x)
+
+
+def allclose(x, y):
+    return assert_allclose(to_numpy(x), to_numpy(y))
 
 
 def approx(x, y, digits=7):
