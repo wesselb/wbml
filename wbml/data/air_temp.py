2991203e0731d7f05084983608b7692dbc1ea760
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2019-10-29 21:26:31 +0000

    Clean data, fix air_temp, and fix toy_sines

--- wbml/data/air_temp.py
+++ wbml/data/air_temp.py
@@ -2,13 +2,24 @@
 
 from __future__ import absolute_import, division, print_function
 
-import warnings
+import datetime
 
 import numpy as np
+import pandas as pd
 
-from .data import CSVReader, Data, float_converter, time_converter, data_path
+from .data import data_path, split_df
 
 __all__ = ['load']
+
+
+def to_float(x):
+    try:
+        return float(x)
+    except ValueError as e:
+        if x in {'', 'n/a', ' '}:
+            return np.nan
+        else:
+            raise e
 
 
 def load():
@@ -17,63 +28,55 @@ def load():
 
     days = list(range(1, 1 + 31))
     fn = '{site:s}{day:02d}Jul2013.csv'
-    r = CSVReader()
-    r.set_field_group('input')
-    r.add_field('Time', time_converter, 'time')
-    r.set_field_group('output')
-    for name, output_name in features:
-        r.add_field(name, float_converter, output_name)
 
-    x_all, ys_all = None, []
+    dfs = []
     for site in sites:
-        xs_site, ys_site = [], []
         for day in days:
-            mins, y_incomplete = \
-                r.read(data_path('air_temp', fn.format(site=site, day=day)))
+            df = pd.read_csv(data_path('air_temp',
+                                       fn.format(site=site, day=day)))
 
-            # Fill in missing data.
-            x = day + np.arange(0, 24 * 60, 5)[:, None] / float(24 * 60)
-            y = np.zeros((12 * 24, 3))
-            y.fill(np.nan)
-            # Input is at multiples of five minutes.
-            y[(mins[:, 0] / 5).astype(int), :] = y_incomplete
+            # Convert times and dates.
+            index = []
+            for i, row in df.iterrows():
+                day, month, year = map(int, row['Date'].split('/'))
+                hour, minute = map(int, row['Time'].split(':'))
+                dt = datetime.datetime(year, month, day, hour, minute)
+                index.append((site, dt))
 
-            xs_site.append(x)
-            ys_site.append(y)
-
-        x_all = np.concatenate(xs_site, axis=0)
-        ys_all.append(np.concatenate(ys_site, axis=0))
-
-    x = x_all
-    y = np.concatenate(ys_all, axis=1)
+            # Construct desired data frame.
+            index = pd.MultiIndex.from_tuples(index, names=['site', 'date'])
+            df = pd.DataFrame({name_to: list(map(to_float, df[name_from]))
+                               for name_from, name_to in features},
+                              index=index)
+            dfs.append(df)
+    df_all = pd.concat(dfs).unstack('site')
 
     # Clear invalid sensor readings.
-    with warnings.catch_warnings():
-        warnings.simplefilter('ignore')
-        y[y < -1000] = np.nan
+    df_all[df_all < -1000] = np.nan
 
-    # Construct data and reorder outputs.
-    d = Data(x, y,
-             ['{}/{}'.format(site, feature)
-              for site in sites
-              for _, feature in features])
-    d = d.select('Bra/height', 'Sot/height', 'Cam/height', 'Chi/height',
-                 'Bra/speed', 'Sot/speed', 'Cam/speed', 'Chi/speed',
-                 'Bra/temp', 'Sot/temp', 'Cam/temp', 'Chi/temp')[0]
+    def make_range(lower, upper):
+        day = datetime.timedelta(days=1)
+        date_start = df_all.index.min()
+        return date_start + (lower - 1) * day, \
+               date_start + (upper - 1) * day
 
     # Construct data sets of various sizes.
     ds = []
-    for all_interval in [(10, 20), (8, 23), None]:
-        d_all = d if all_interval is None else d.interval(*all_interval)[0]
+    for lower, upper in [make_range(10, 20),
+                         make_range(8, 23),
+                         (df_all.index.min(), df_all.index.max())]:
+        df = df_all[(df_all.index >= lower) & (df_all.index <= upper)]
 
-        # Select test sets.
-        test1, d_train = d_all.interval_y('Cam/temp', 12, 16)
-        test2, d_train = d_train.interval_y('Chi/temp', 14, 18)
+        test1, train = split_df(df, make_range(12, 16),
+                                [('temp', 'Cam')])
+        test2, train = split_df(train, make_range(14, 18),
+                                [('temp', 'Chi')])
 
-        # Surround the test sets a bit for nice predictions.
-        test1_ext = d.interval_y('Cam/temp', 11.5, 16.5)[0]
-        test2_ext = d.interval_y('Chi/temp', 13.5, 18.5)[0]
+        test1_ext, _ = split_df(df, make_range(11.5, 16.5),
+                                [('temp', 'Cam')])
+        test2_ext, _ = split_df(train, make_range(13.5, 18.5),
+                                [('temp', 'Chi')])
 
-        ds.append((d_all, d_train, [test1, test2, test1_ext, test2_ext]))
+        ds.append((df, train, [test1, test2, test1_ext, test2_ext]))
 
     return ds
--- wbml/data/toy_sines.py
+++ wbml/data/toy_sines.py
@@ -3,8 +3,9 @@
 from __future__ import absolute_import, division, print_function
 
 import numpy as np
+import pandas as pd
 
-from .data import Data
+from .data import split_df
 
 __all__ = ['load']
 
@@ -19,9 +20,13 @@ def load():
     y = y + 0.2 * np.random.randn(*y.shape)
 
     # Construct data and reorder outputs.
-    d_all = Data(x, y, ['sin(x)', 'x^2', 'sin(x)^2', 'x + sin(x)'])
+    df = pd.DataFrame(y,
+                      index=pd.Index(x, name='time'),
+                      columns=['sin(x)', 'x^2', 'sin(x)^2', 'x + sin(x)'])
 
     # Select data from the paper.
-    d_test, d_train = d_all.interval_y(['sin(x)^2', 'x + sin(x)'],
-                                       4 * np.pi, 6 * np.pi)
-    return d_all, d_train, [d_test]
+    df_train, df_test = split_df(df,
+                                 (4 * np.pi, 6 * np.pi + 1),
+                                 ['sin(x)^2', 'x + sin(x)'])
+
+    return df, df_train, df_test
