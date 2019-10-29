fd08fa21c87e8738332db4361a1e0c07272155c1
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2019-10-29 20:22:30 +0000

    Fix VI FA experiment

--- scripts/vi_fa.py
+++ scripts/vi_fa.py
@@ -1,39 +1,42 @@
-import numpy as np
+import lab.tensorflow as B
 import tensorflow as tf
-from lab.tf import B
-from stheno.tf import Normal, Diagonal, UniformDiagonal
-
-from wbml import elbo, vars32
-
-s = tf.Session()
+import wbml.out as out
+from stheno.tensorflow import Normal, Diagonal
+from varz.tensorflow import Vars, minimise_adam
+from wbml.vi import elbo
 
 d = 2
+vs = Vars(tf.float32)
 
 # Define a prior and likelihood and generate data.
-prior = Normal(Diagonal(np.linspace(1, 2, d, dtype=np.float32)))
-lik_noise = Normal(UniformDiagonal(np.array(.1, dtype=np.float32), d))
-z_true = s.run(prior.sample())
-y = s.run((lik_noise + z_true).sample(100))
+prior = Normal(Diagonal(B.linspace(1, 2, d)))
+lik_noise = Normal(Diagonal(0.1 * B.ones(d)))
+z_true = prior.sample()
+y = (lik_noise + z_true).sample(100)
 
-# Define likelihood and q distribution.
-lik = lambda x: B.sum((lik_noise + y).logpdf(x))
-q = Normal(Diagonal(vars32.positive(shape=[d])), vars32.get(shape=[d, 1]))
+
+# Define likelihood.
+def lik(x):
+    return B.sum((lik_noise + y).logpdf(x))
+
+
+# Construct q distribution.
+def construct_q(vs):
+    q = Normal(Diagonal(vs.positive(shape=[d], name='q/var')),
+               vs.get(shape=[d, 1], name='q/mean'))
+    return q
+
 
 # Construct objective.
-obj = -elbo(lik, prior, q, num_samples=1)
+def objective(vs):
+    q = construct_q(vs)
+    return -elbo(lik, prior, q, num_samples=1)
+
 
 # Optimise.
-opt = tf.train.AdamOptimizer(1e-2).minimize(obj)
-s.run(tf.global_variables_initializer())
-for i in range(5000):
-    _, val = s.run([opt, obj])
-    if i % 100 == 0:
-        print(i, val)
+objective_compiled = tf.function(objective, autograph=False)
+minimise_adam(objective_compiled, vs, trace=True, iters=2000, rate=5e-2)
 
 # Print result.
-print('True', z_true.flatten())
-sample = q.sample(1000)
-z_est = B.mean(sample, axis=1)
-z_est_std = B.mean((sample - z_est[:, None]) ** 2, axis=1) ** .5
-print('Mean estimate', s.run(z_est))
-print('Std estimate', s.run(z_est_std))
+out.kv('True', z_true)
+out.kv('Mean q', construct_q(vs).mean)
