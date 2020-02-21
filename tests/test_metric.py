d83bd8855f9368cb70281d048c33598c7551d5f7
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2020-02-21 20:39:51 +0000

    Add and test metric

---
+++ tests/test_metric.py
@@ -0,0 +1,71 @@
+import pytest
+import wbml.metric
+import pandas as pd
+import lab as B
+
+two_arg = ['mse', 'smse', 'rmse', 'srmse', 'r2']
+three_arg = ['mll', 'smll']
+
+two_arg_standardised = [('smse', 1), ('srmse', 1)]
+three_arg_standardised = [('smll', 0)]
+
+
+@pytest.mark.parametrize('name', two_arg)
+def test_two_arg(name):
+    metric = getattr(wbml.metric, name)
+
+    # Test scalar usage.
+    assert isinstance(metric(1, B.randn(10)), B.Number)
+
+    # Test series usage.
+    assert isinstance(metric(B.randn(10), B.randn(10)), B.Number)
+
+    # Test matrix usage.
+    assert isinstance(metric(B.randn(10, 10), B.randn(10, 10)), pd.Series)
+
+    # Check that higher-order tensors fail.
+    with pytest.raises(ValueError):
+        metric(B.randn(10, 10, 10), B.randn(10, 10, 10))
+
+
+@pytest.mark.parametrize('name', three_arg)
+def test_three_arg(name):
+    metric = getattr(wbml.metric, name)
+
+    # Test scalar usage.
+    assert isinstance(metric(1, 1, B.randn(10)), B.Number)
+
+    # Test series usage.
+    assert isinstance(metric(B.randn(10),
+                             B.rand(10),
+                             B.randn(10)),
+                      B.Number)
+
+    # Test matrix usage.
+    assert isinstance(metric(B.randn(10, 10),
+                             B.rand(10, 10),
+                             B.randn(10, 10)),
+                      pd.Series)
+
+    # Check that higher-order tensors fail.
+    with pytest.raises(ValueError):
+        metric(B.randn(10, 10, 10),
+               B.rand(10, 10, 10),
+               B.randn(10, 10, 10))
+
+
+@pytest.mark.parametrize('name, neutral_value', two_arg_standardised)
+def test_two_arg_standardised(name, neutral_value):
+    metric = getattr(wbml.metric, name)
+    x = B.randn(10)
+    mean = x.mean() * B.ones(10)
+    assert B.all(metric(mean, x) == neutral_value)
+
+
+@pytest.mark.parametrize('name, neutral_value', three_arg_standardised)
+def test_three_arg_standardised(name, neutral_value):
+    metric = getattr(wbml.metric, name)
+    x = B.randn(10)
+    mean = x.mean() * B.ones(10)
+    variance = x.var(ddof=0) * B.ones(10)
+    assert B.all(metric(mean, variance, x) == neutral_value)
---
+++ wbml/metric.py
@@ -0,0 +1,145 @@
+from functools import wraps
+
+import lab as B
+import numpy as np
+import pandas as pd
+from plum import Dispatcher, Union
+
+__all__ = ['mse', 'smse',
+           'rmse', 'srmse',
+           'mll', 'smll',
+           'r2']
+
+_dispatch = Dispatcher()
+_Pandas = Union(pd.DataFrame, pd.Series)
+_PandasOrScalar = Union(_Pandas, B.Number)
+
+
+def _auto_convert(f):
+    @_dispatch([B.Numeric])
+    @wraps(f)
+    def _convert(*args):
+        converted_args = ()
+        for i, arg in enumerate(args):
+            if B.rank(arg) == 0:
+                converted_args += (arg,)
+            elif B.rank(arg) == 1:
+                converted_args += (pd.Series(arg),)
+            elif B.rank(arg) == 2:
+                converted_args += (pd.DataFrame(arg),)
+            else:
+                raise ValueError(f'Argument {i} has rank {B.rank(arg)}, which '
+                                 f'cannot be automatically converted to a '
+                                 f'Pandas object.')
+
+        return f(*converted_args)
+
+    return f
+
+
+@_auto_convert
+@_dispatch(_PandasOrScalar, _Pandas)
+def mse(mean, data):
+    """Mean squared error.
+
+    Args:
+        mean (tensor): Mean of prediction.
+        data (tensor): Reference data.
+
+    Returns:
+        tensor: Mean squared error.
+    """
+    return ((mean - data) ** 2).mean()
+
+
+@_auto_convert
+@_dispatch(_PandasOrScalar, _Pandas)
+def smse(mean, data):
+    """Standardised mean squared error.
+
+    Args:
+        mean (tensor): Mean of prediction.
+        data (tensor): Reference data.
+
+    Returns:
+        tensor: Standardised mean squared error.
+    """
+    return mse(mean, data) / mse(data.mean(), data)
+
+
+@_auto_convert
+@_dispatch(_PandasOrScalar, _Pandas)
+def rmse(mean, data):
+    """Root mean squared error.
+
+    Args:
+        mean (tensor): Mean of prediction.
+        data (tensor): Reference data.
+
+    Returns:
+        tensor: Root mean squared error.
+    """
+    return mse(mean, data) ** .5
+
+
+@_auto_convert
+@_dispatch(_PandasOrScalar, _Pandas)
+def srmse(mean, data):
+    """Standardised root mean squared error.
+
+    Args:
+        mean (tensor): Mean of prediction.
+        data (tensor): Reference data.
+
+    Returns:
+        tensor: Standardised root mean squared error.
+    """
+    return rmse(mean, data) / rmse(data.mean(), data)
+
+
+@_auto_convert
+@_dispatch(_PandasOrScalar, _PandasOrScalar, _Pandas)
+def mll(mean, variance, data):
+    """Mean log loss.
+
+    Args:
+        mean (tensor): Mean of prediction.
+        variance (tensor): Variance of prediction.
+        data (tensor): Reference data.
+
+    Returns:
+        tensor: Mean log loss.
+    """
+    return (0.5 * np.log(2 * np.pi) +
+            0.5 * (mean - data) ** 2 / variance).mean()
+
+
+@_auto_convert
+@_dispatch(_PandasOrScalar, _PandasOrScalar, _Pandas)
+def smll(mean, variance, data):
+    """Standardised mean log loss.
+
+    Args:
+        mean (tensor): Mean of prediction.
+        variance (tensor): Variance of prediction.
+        data (tensor): Reference data.
+
+    Returns:
+        tensor: Standardised mean log loss.
+    """
+    return mll(mean, variance, data) - mll(data.mean(), data.var(ddof=0), data)
+
+
+@_auto_convert
+@_dispatch(_PandasOrScalar, _Pandas)
+def r2(mean, data):
+    """R-squared.
+
+    Args:
+        mean (tensor): Mean of prediction.
+        data (tensor): Reference data.
+
+    Returns:
+        tensor: R-squared.
+    """
+    return 1 - ((data - mean) ** 2).mean() / data.var(ddof=0)
